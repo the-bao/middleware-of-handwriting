965e2084030f19448f6f0d93fe075e33e9f56d73
the-bao <785909186@qq.com>
2025-08-10 16:40:24 +0800

    feat(增加ctl线程池状态控制):通过ctl来控制线程数，保证线程创建符合预期

--- thread_pool/src/main/java/io/github/MyThreadPool.java
+++ thread_pool/src/main/java/io/github/MyThreadPool.java
@@ -1,8 +1,11 @@
 package io.github;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author rty
@@ -31,11 +34,51 @@ public class MyThreadPool {
     // 最大线程数
     int maxSize;
 
-    // 核心线程队列
-    List<Worker> coreThreadList;
+    // 增加worker时加锁
+    private final ReentrantLock mainLock = new ReentrantLock();
 
-    // 辅助线程队列
-    List<Worker> supportThreadList;
+    // 线程池最大线程数 （监控使用）
+    private int largestPoolSize;
+
+    // 线程集合 （监控使用）
+    private final HashSet<Worker> workers = new HashSet<>();
+
+    /*
+    * 状态控制
+    * ctl 32位 低29位COUNT_BITS用于存储线程池的数量
+    * 高3位用于存储线程状态
+    * 111 RUNNING
+    * 000 SHUTDOWN
+    * 001 STOP
+    * 010 TIDYING
+    * 011 TERMINATED
+    * */
+    private final AtomicInteger ctl = new AtomicInteger(getCtl(RUNNING,0));
+    private static final int COUNT_BITS = Integer.SIZE - 3;
+    private static final int CAPACITY   = (1 << COUNT_BITS) - 1;
+
+    private static final int RUNNING = -1 << COUNT_BITS;
+    private static final int SHUTDOWN = 0 << COUNT_BITS;
+    private static final int STOP = 1 << COUNT_BITS;
+    private static final int TIDYING = 2 << COUNT_BITS;
+    private static final int TERMINATED = 3 << COUNT_BITS;
+
+    /*
+    * 通过与运算得到想要的状态位 和1相与都是1，和0相与得到自己
+    * 11111111111111111111111111111 CAPACITY
+    * 通过或运算得到想要的ctl字段 0和任何数或都是0
+    * */
+    private static int getRunState(int ctl){
+        return ctl & ~CAPACITY;
+    }
+
+    private static int getWorkCount(int ctl){
+        return ctl & CAPACITY;
+    }
+
+    private static int getCtl(int runState, int workCount){
+        return runState | workCount;
+    }
 
     // 阻塞队列 存放任务
     private final BlockingQueue<Runnable> blockingQueue;
@@ -52,8 +95,6 @@ public class MyThreadPool {
         this.timeUnit = timeUnit;
         this.threadFactory = Executors.defaultThreadFactory();
         this.allowCoreThreadTimeOut = false;
-        coreThreadList = new CopyOnWriteArrayList<>();
-        supportThreadList = new CopyOnWriteArrayList<>();
     }
 
     public MyThreadPool(int corePoolSize, int maxSize, BlockingQueue<Runnable> blockingQueue,RejectHandler rejectHandler,int keepAliveTime,TimeUnit timeUnit,ThreadFactory threadFactory) {
@@ -65,8 +106,6 @@ public class MyThreadPool {
         this.timeUnit = timeUnit;
         this.threadFactory = threadFactory;
         this.allowCoreThreadTimeOut = false;
-        coreThreadList = new CopyOnWriteArrayList<>();
-        supportThreadList = new CopyOnWriteArrayList<>();
     }
 
     public ThreadFactory getThreadFactory() {
@@ -86,40 +125,78 @@ public class MyThreadPool {
     * */
     void execute(Runnable task){
         // 1.核心线程没满，则创建一个核心线程执行任务
-        if (coreThreadList.size() < corePoolSize){
-            Worker worker = new Worker(task);
-            coreThreadList.add(worker);
-            final Thread t = worker.thread;
-            t.start();
-            System.out.println(t.getName() + ":核心工作线程创建");
-            return;
+        if (getWorkCount(ctl.get()) < corePoolSize){
+            if (addWorker(task,true)) return;
         }
         // 2.核心线程满了，则直接放入阻塞任务队列中等待执行
         if (blockingQueue.offer(task)){
             System.out.println(Thread.currentThread().getName() + "的任务进入阻塞队列");
             // 检查是否需要补充线程
-            if (coreThreadList.isEmpty() && coreThreadList.size() < corePoolSize) {
-                Worker worker = new Worker(null);
-                coreThreadList.add(worker);
-                final Thread t = worker.thread;
-                t.start();
-                System.out.println(t.getName() + ":核心工作线程创建");
+            if (getWorkCount(ctl.get()) < corePoolSize) {
+                addWorker(null,true);
             }
             return;
         }
         // 3.核心线程满了，阻塞队列满了，但是辅助线程未满，则创建一个辅助线程执行任务
-        if (coreThreadList.size() + supportThreadList.size() < maxSize){
-            Worker worker = new Worker(task);
-            supportThreadList.add(worker);
-            final Thread t = worker.thread;
-            t.start();
-            System.out.println(t.getName() + ":辅助工作线程创建");
-            return;
+        if (ctl.get() < maxSize){
+            if (addWorker(task,false)){
+                return;
+            }
         }
         // 4.核心线程满了，阻塞队列满了，辅助线程也满了，则执行拒绝策略
         // 执行拒绝策略
         System.out.println("执行拒绝策略");
         rejectHandler.reject(task, this);
+    }
+
+    private boolean addWorker(Runnable firstTask, boolean core){
+        retry:
+        for (;;){
+            int c = ctl.get();
+            int rs = getRunState(c);
+            int wc = getWorkCount(c);
+
+            // 检查线程池的状态 所有大于等于SHUTDOWN的状态和SHUNTOWN处理新任务时都拒绝线程创建
+            if (rs >= SHUTDOWN && (rs == SHUTDOWN && firstTask != null)) return false;
+
+            for (;;){
+                if (wc >= CAPACITY || wc >= (core ? corePoolSize : maxSize)) return false;
+                if (ctl.compareAndSet(c,c+1)) break retry;
+                c = ctl.get();
+                if (getRunState(c) != rs) continue retry;
+            }
+        }
+
+        boolean workerStarted = false;
+        boolean workerAdded = false;
+        Worker w = null;
+        try {
+            w = new Worker(firstTask);
+            final Thread t = w.thread;
+            if (t != null){
+                final ReentrantLock mainLock = this.mainLock;
+                mainLock.lock();
+                try {
+                    int rs = getRunState(ctl.get());
+                    // 仅允许RUNNING或者SHUTDOWN处理队列中剩余任务
+                    if (rs == RUNNING || rs == SHUTDOWN && firstTask == null){
+                        if (t.isAlive()) throw new IllegalThreadStateException();
+                        workers.add(w);
+                        workerAdded = true;
+                    }
+                }finally {
+                    mainLock.unlock();
+                }
+                if (workerAdded){
+                    t.start();
+                    workerStarted = true;
+                }
+            }
+        }finally {
+            System.out.println("线程创建：" + workerStarted);
+            System.out.println("当前线程数：" + workers.size());
+        }
+        return workerStarted;
     }
 
     /*
@@ -148,8 +225,8 @@ public class MyThreadPool {
                 // 如果没有初始任务，则从队列中获取任务
                 if (task == null) {
                     try {
-                        // 根据是否为核心线程和是否允许超时来决定获取任务的方式
-                        boolean isCoreThread = coreThreadList.contains(this);
+                        // 根据是否为核心线程和是否允许超时来决定获取任务的方式 , 线程之间是平等的 , 是否核心取决于线程数量
+                        boolean isCoreThread = getWorkCount(ctl.get()) <= corePoolSize;
                         if (isCoreThread && !allowCoreThreadTimeOut) {
                             // 核心线程且不允许超时，使用阻塞获取
                             task = blockingQueue.take();
@@ -179,8 +256,6 @@ public class MyThreadPool {
                 }
             }
             // 从线程列表中移除
-            coreThreadList.remove(this);
-            supportThreadList.remove(this);
             System.out.println(Thread.currentThread().getName() + ":工作线程被回收");
         }
 
